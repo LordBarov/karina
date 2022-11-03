f0b46474647d4ac3dbaf01fddb23f06b28ae311a
LordBarov <arnurlord@gmail.com>
2022-11-03 22:47:36 +0600

    daw

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ApexChart from "./ApexChart";
+import ApexChart2 from "./ApexChart2";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ApexChart/>
+        <ApexChart2 />
     </div>
   );
 }
